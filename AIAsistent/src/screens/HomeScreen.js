0027a091fb384d1439c43cff7c3f1e45e6b1def4
artemshakun <artem.shakun.91@gmail.com>
2023-09-11 11:14:49 +0300

    initial-commit

---
+++ AIAsistent/src/screens/HomeScreen.js
@@ -0,0 +1,86 @@
+import React, {useState} from 'react';
+import {View, Text, SafeAreaView, Image, ScrollView} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import Features from '../components/features';
+import {dummyMessages} from '../constatns';
+
+export default function HomeScreen() {
+  const [messages, setMessages] = useState(dummyMessages);
+  return (
+    <View className="flex-1 bg-white">
+      <SafeAreaView className="flex-1 flex mx-5">
+        {/* bot icon */}
+        <View className="flex-row justify-center">
+          <Image
+            source={require('../../assets/images/bot.png')}
+            style={{height: hp(15), width: hp(15)}}
+          />
+        </View>
+        {/* features || messages */}
+        {messages.length > 0 ? (
+          <View className="space-y-2 flex-1">
+            <Text
+              style={{fontSize: wp(5)}}
+              className="text-gray-700 font-semibold ml-1">
+              Assistant
+            </Text>
+            <View
+              style={{height: hp(58)}}
+              className="bg-neutral-200 rounded-3xl p-4">
+              <ScrollView
+                bounces={false}
+                className="space-y-4"
+                showsVerticalScrollIndicator={false}>
+                {messages.map((messages, index) => {
+                  if (messages.role === 'assistant') {
+                    if (messages.content.includes('https')) {
+                      // its an AI image
+                      return (
+                        <View key={index} className="flex-row justify-start">
+                          <View className="p-2 flex rounded-2xl bg-emerald-100 rounded-tl-none">
+                            <Image
+                              source={{uri: messages.content}}
+                              className="rounded-2xl"
+                              resizeMethod="contain"
+                              style={{height: wp(60), width: wp(60)}}
+                            />
+                          </View>
+                        </View>
+                      );
+                    } else {
+                      // text response
+                      return (
+                        <View
+                          key={index}
+                          style={{width: wp(70)}}
+                          className="bg-emerald-100 rounded-xl p-2 rounded-tl-none">
+                          <Text>{messages.content}</Text>
+                        </View>
+                      );
+                    }
+                  } else {
+                    // user input
+                    return (
+                      <View key={index} className="flex-row justify-end">
+                        <View
+                          style={{width: wp(70)}}
+                          className="bg-white rounded-xl p-2 rounded-tr-none">
+                          <Text>{messages.content}</Text>
+                        </View>
+                      </View>
+                    );
+                  }
+                })}
+              </ScrollView>
+            </View>
+          </View>
+        ) : (
+          <Features />
+        )}
+      </SafeAreaView>
+    </View>
+  );
+}
