70f23688812fa9525c1059264873b51c091ca247
artemshakun <artem.shakun.91@gmail.com>
2023-09-12 09:06:38 +0300

    conect voice handler

--- AIAsistent/src/screens/HomeScreen.js
+++ AIAsistent/src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
@@ -13,19 +13,72 @@ import {
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import Features from '../components/features';
-import {dummyMessages} from '../constatns';
+import {dummyMessages} from '../constants';
+import Voice from '@react-native-community/voice';
 
 export default function HomeScreen() {
   const [messages, setMessages] = useState(dummyMessages);
   const [recording, setRecording] = useState(false);
   const [speaking, setSpeaking] = useState(false);
 
+  const speechStartHandler = e => {
+    console.log('speech start handler');
+  };
+
+  const speechEndHandler = e => {
+    setRecording(false);
+    console.log('speech end handler');
+  };
+
+  const speechResultsHandler = e => {
+    console.log('voice event: ', e);
+  };
+
+  const speechErrorHandler = e => {
+    console.log('speech error handler: ', e);
+  };
+
+  const startRecording = async () => {
+    setRecording(true);
+    try {
+      await Voice.start('en-GB'); //en-US
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  };
+
+  const stopRecording = async () => {
+    setRecording(true);
+    try {
+      await Voice.stop(); //en-US
+      setRecording(false);
+      // fetch response
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  };
+
   const clear = () => {
     setMessages([]);
   };
+
   const stopSpeaking = () => {
     setSpeaking(false);
   };
+
+  useEffect(() => {
+    // voice handler events
+    Voice.onSpeechStart = speechStartHandler;
+    Voice.onSpeechEnd = speechEndHandler;
+    Voice.onSpeechResults = speechResultsHandler;
+    Voice.onSpeechError = speechErrorHandler;
+
+    return () => {
+      // destroy voice
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
   return (
     <View className="flex-1 bg-white">
       <SafeAreaView className="flex-1 flex mx-5">
@@ -61,7 +114,7 @@ export default function HomeScreen() {
                             <Image
                               source={{uri: messages.content}}
                               className="rounded-2xl"
-                              resizeMethod="contain"
+                              resizeMethod="scale"
                               style={{height: wp(60), width: wp(60)}}
                             />
                           </View>
@@ -100,7 +153,8 @@ export default function HomeScreen() {
         {/* recording, clear and stop buttons */}
         <View className="flex justify-center items-center">
           {recording ? (
-            <TouchableOpacity>
+            <TouchableOpacity onPress={stopRecording}>
+              {/* recording stop button */}
               <Image
                 className="rounded-full"
                 source={require('../../assets/images/voiceLoading.gif')}
@@ -108,7 +162,8 @@ export default function HomeScreen() {
               />
             </TouchableOpacity>
           ) : (
-            <TouchableOpacity>
+            <TouchableOpacity onPress={startRecording}>
+              {/* recording start button */}
               <Image
                 className="rounded-full"
                 source={require('../../assets/images/recordingIcon.png')}
